082d25290a4932aa0b94afdbb9f9cf463140f0d2
Christopher Columbus <100056225+alva333@users.noreply.github.com>
2024-08-28 11:12:55 -0400

    test commit for deployment workflow

--- flask/app/models/models.py
+++ flask/app/models/models.py
@@ -6,8 +6,8 @@ class User(db.Model):
     username =      db.Column(db.String(32),    nullable=False, index=True, unique=True)
     password_hash = db.Column(db.String(128),   nullable=False)
     created_at =    db.Column(db.DateTime,      nullable=False)
-    active =        db.Column(db.DateTime,      nullable=False)
+    active =        db.Column(db.Boolean,      nullable=False)
 
     def __str__(self):
-        return f"User {self.id} ({self.username}), age {self.age}, registered on {self.created_at}"
+        return f"User {self.id} ({self.username}), registered on {self.created_at} -- status: {self.active}"
     
